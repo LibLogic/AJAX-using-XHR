e742300c908c84e03982df420f0181ac1af67530
LibLogic <hdgknsn@gmail.com>
2019-02-07 15:07:15 +0000

    updated process.php file

--- process.php
+++ process.php
@@ -1,9 +1,19 @@
 <?php
 
+// connect to a database
+$conn = mysqli_connect('localhost', 'hdgknsn', '', 'ajax_test', 3306);
+
 echo 'Processing...';
 
 if(isset($_POST['name'])){
-	echo 'POST: your name is ' . $_POST['name'];
+	$name = mysqli_real_escape_string($conn, $_POST['name']);
+	// echo 'POST: your name is ' . $_POST['name'];
+	$query = "INSERT INTO users(name) VALUES('$name')";
+	if(mysqli_query($conn, $query)){
+		echo 'User Added...';
+	} else {
+		echo 'Error: ' . mysqli_error($conn);
+	}
 }
 
 if(isset($_GET['name'])){
